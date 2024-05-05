ea6ec0e6a883eb4cf2c2d494305861787573bf3d
SomaBirajdar <somabiraj111@gmail.com>
2024-05-05 22:50:30 +0530

    add two number addition code

---
+++ src/arithmatic_operation/Addition.java
@@ -0,0 +1,10 @@
+package arithmatic_operation;
+
+public class Addition {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 6;
+        int z = x + y;
+        System.out.println("The sum is = " + z);
+    }
+}
