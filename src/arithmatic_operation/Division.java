a346a6cd01cdb750a3f6099e3452a3810e37debd
SomaBirajdar <somabiraj111@gmail.com>
2024-05-05 23:46:07 +0530

    two number division

---
+++ src/arithmatic_operation/Division.java
@@ -0,0 +1,10 @@
+package arithmatic_operation;
+
+public class Division {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        int div = x / y;
+        System.out.println("The quotient is " + div);
+    }
+}
