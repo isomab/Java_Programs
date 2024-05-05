fa1b63cf0a21659d27eb9e097f705c82f56bad35
SomaBirajdar <somabiraj111@gmail.com>
2024-05-05 23:00:24 +0530

    add two number multiplication program

---
+++ src/arithmatic_operation/Multiplication.java
@@ -0,0 +1,10 @@
+package arithmatic_operation;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 4;
+        int multiplicaton = x * y;
+        System.out.println("The multiplication of two numbers is " + multiplicaton);
+    }
+}
