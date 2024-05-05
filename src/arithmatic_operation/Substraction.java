16de6c2c4babd57218fa5b1710571a27e1bfc3d0
SomaBirajdar <somabiraj111@gmail.com>
2024-05-05 22:56:22 +0530

    add two number substraction

--- src/arithmatic_operation/Substraction.java
+++ src/arithmatic_operation/Substraction.java
@@ -1,2 +1,10 @@
-package arithmatic_operation;public class Substraction {
+package arithmatic_operation;
+
+public class Substraction {
+    public static void main(String[] args) {
+        int x = 25;
+        int y = 13;
+        int z = x - y;
+        System.out.println("The Substraction of two numbers is = " + z);
+    }
 }
