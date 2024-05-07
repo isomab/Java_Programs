fdef61c58d68dbaeb5c8f32c1a07a82c138e5c33
SomaBirajdar <somabiraj111@gmail.com>
2024-05-07 22:50:07 +0530

    updated code

--- src/interview/Palindrom.java
+++ src/interview/Palindrom.java
@@ -1,20 +1,18 @@
 package interview;
 
-
-
 public class Palindrom {
     public static void main(String[] args) {
         String number = "12321";
         char[] numberArray = number.toCharArray();
-        int maxLength = number.length() - 1;
+        int updatedLength = number.length() - 1;
 
         for (int i = 0; i < number.length(); i++) {
-            if (numberArray[i] != numberArray[maxLength]) {
+            if (numberArray[i] != numberArray[updatedLength]) {
                 System.out.println("Given Number "+number+" is not palindrom");
                 break;
             }
-            maxLength--;
-            if (i == ((number.length() / 2) + 1)) {
+            updatedLength--;
+            if (i == (number.length()-1)) {
                 System.out.println("Given Number "+number+" is palindrom");
             }
         }
